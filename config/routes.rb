a0818c7d9d665a33a2a9701c4e0d99bdbbf64195
Ferdinand Vermersch <ferdi.vermersch@gmail.com>
2019-11-25 17:04:29 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+
+  resources :subjects do
+    resources :class_memberships, only: [:create, :destroy, :index]
+  end
+
+  resources :appoinments, only: [:show, :index, :destroy, :edit, :update]
+
+  resources :users, only: [:show] do
+    resources :appointments, only: [:create]
+  end
+
+  resources :daily_emotions, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
