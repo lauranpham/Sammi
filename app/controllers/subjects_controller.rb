b79bb2bed3682795df363a7677af1ad9237040dd
Ferdinand Vermersch <ferdi.vermersch@gmail.com>
2019-11-26 12:21:35 +0000

    more stuff

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,17 +1,26 @@
 class SubjectsController < ApplicationController
   def index
-    @subjects = Subject.all
+    @user = current_user
+    @subjects = @user.subjects if @user.teacher?
   end
 
   def show
-
+    @subject = Subject.find(params[:id])
   end
 
   def new
-
+    @user = current_user
   end
 
   def create
+    @subject = Subject.new(subject_params)
+    @subject.user = current_user
+    @subject.pet = Pet.find(params[:pet_id])
+    if @subject.save
+      redirect_to subjects_path
+    else
+      redirect_to pet_path(@subject.pet)
+    end
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,6 @@ class User < ApplicationRecord
   has_many :class_memberships, through: :subjects
   has_many :students, through: :class_memberships, source: :student
 
-
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :teacher, presence: true
 end
-
