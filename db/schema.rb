13de7f654d353c3a02ed86cce1a14dcc2a89add5
Laura Pham <l.pham2311@gmail.com>
2019-11-26 12:44:46 +0000

    Finished seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_211524) do
+ActiveRecord::Schema.define(version: 2019_11_26_122849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,12 +41,13 @@ ActiveRecord::Schema.define(version: 2019_11_25_211524) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["emotion_id"], name: "index_daily_emotions_on_emotion_id"
     t.index ["user_id"], name: "index_daily_emotions_on_user_id"
   end
 
   create_table "emotions", force: :cascade do |t|
-    t.integer "rating"
+    t.integer "rating", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
