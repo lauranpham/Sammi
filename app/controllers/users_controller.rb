79329e940f3b0db2d002d3c3767ce22c310c73fe
Laura Pham <l.pham2311@gmail.com>
2019-11-27 15:56:10 +0000

    Added appointment form to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
     @daily_emotions = @student.daily_emotions
     @dailynumber = @daily_emotions.map { |emo| [emo.date, emo.emotion.number] }
     authorize current_user
+    @appointment = Appointment.new
   end
 end
