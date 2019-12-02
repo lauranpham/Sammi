a8e3f9e37fd3c078b783d7c52eb1e21cbeb7f76d
Laura Pham <l.pham2311@gmail.com>
2019-12-02 16:38:21 +0000

    Added color scheme to calendar

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -24,12 +24,17 @@ class Emotion < ApplicationRecord
   def color
     num = self.number
     case num
-    when 1 then '#A33141'
-    when 2 then '#F08853'
-    when 3 then '#EEDD9D'
-    when 4 then '#6B92AB'
-    when 5 then '#D0FBDF'
+    when 1 then '#F25855'
+    when 2 then '#F5785A'
+    when 3 then '#F7955F'
+    when 4 then '#F7B363'
+    when 5 then '#F7D167'
     end
   end
-
 end
+
+    # when 1 then '#A33141'
+    # when 2 then '#F08853'
+    # when 3 then '#EEDD9D'
+    # when 4 then '#6B92AB'
+    # when 5 then '#D0FBDF'
