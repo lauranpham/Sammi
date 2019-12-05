ca41cc9f43a96ad108caa16d09e1f184428c50dc
David Belka <david.belka@yahoo.co.uk>
2019-12-05 12:40:50 +0000

    finished

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,23 +1,25 @@
 class NotesController < ApplicationController
-  def index
-    @notes = Note.all
-  end
-
-  def new
-    @note = Note.create(params[:id])
-    @user = User.find(params[:id])
-  end
-
   def create
-    Noto
-  end
-
-  def edit
-  end
-
-  def update
+    @note = Note.new(note_params)
+    @note.teacher = current_user
+    @note.student = User.find(params[:user_id])
+    authorize @note
+    if @note.save
+      redirect_to subject_user_path(Subject.find(params[:subject_id]), @note.student)
+    else
+      render 'users/show'
+    end
   end
 
   def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to subject_user_path(Subject.find(params[:subject_id]), @note.student)
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content)
   end
 end
