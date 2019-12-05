0cce19b8524b328962569003f2f0e3dcb919d451
David Belka <david.belka@yahoo.co.uk>
2019-12-05 17:52:07 +0000

    ajaxified notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,12 +2,20 @@ class NotesController < ApplicationController
   def create
     @note = Note.new(note_params)
     @note.teacher = current_user
+    @subject = Subject.find(params[:subject_id])
+    @student = User.find(params[:user_id])
     @note.student = User.find(params[:user_id])
     authorize @note
     if @note.save
-      redirect_to subject_user_path(Subject.find(params[:subject_id]), @note.student)
+      respond_to do |format|
+        format.html { redirect_to subject_user_path(Subject.find(params[:subject_id]), @note.student) }
+        format.js
+      end
     else
-      render 'users/show'
+      respond_to do |format|
+        format.html { render 'users/show' }
+        format.js
+      end
     end
   end
 
